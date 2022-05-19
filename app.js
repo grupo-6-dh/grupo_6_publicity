e32a1d49a37a724637940b1f6fb98559239d9423
hernanc411 <hernan_c_94@hotmail.com>
2022-05-19 17:32:12 -0300

    Se creo la vista formulario de registro

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const express=require("express");
-const path=require("path");
+const express = require('express');
+const path = require('path');
 const app=express();
+const publicPath=path.resolve(__dirname,"./public");
 
-const publicPath=path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
-
-app.listen(8080,()=> {
-    console.log("Servidor corriendo en el puerto 8080")
+app.listen(3030,()=> {
+    console.log("Servidor corriendo en el puerto 3030")
 });
 
-
 app.get('/',function(req,res){
-    res.sendFile(path.resolve(__dirname,'./views/index.html'));
+    res.sendFile(path.join(__dirname,'/view/index.html'));
 })
