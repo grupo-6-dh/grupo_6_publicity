8550718d8343d515e1bff5df0f66553e6bdd7047
Silvestre525 <silvestrevillar02@gmail.com>
2022-06-22 19:16:33 -0300

    Se agregaron rutas de usuarios y controladores de usuarios

---
+++ src/controller/controllerUsers.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+/*
+const controllerUsers = {
+    registro: (req, res) => {
+        return res.render('registro');
+    },
+
+    detalleProducto: (req, res) => {
+        return res.render('detalle-producto');
+    }
+}
+
+module.exports = controllerUsers;
+*/
---
+++ src/routes/indexUsers.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const routesUsers = express.Router();
+const controller = require("../controller/controller")
+
+//---rutas---
+routesUsers.get("/")
+
+routesUsers.get("/registro",)
+
+
+module.exports = routesUsers;
+
