f0a98d5cc711e6d2f02decafca5e83951b26f347
hernanc411 <hernan_c_94@hotmail.com>
2022-09-14 16:57:42 -0300

    Se creo la vista principal del dashboard

---
+++ dashboard/src/Components/ejemploPanel/Cantidad.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Cantidad.css';
+
+
+function Cantidad(){
+    return(
+        <div className="panel">
+            <h1>Ejemplo de panel</h1>
+            <hr />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel expedita ea sunt quia ipsum quo eveniet. Unde, numquam atqu</p>
+        </div>
+    )
+}
+
+export default Cantidad;
